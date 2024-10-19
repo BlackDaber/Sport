4143b960da9913b603ef2a292e536abe5637e4a2
BlackDaber <blackdaber96@mail.ru>
2024-10-20 00:29:18 +0400

    feat: add basic training program crud operations

---
+++ src/main/java/gg/blackdaber/sport/anotation/SportApiMapping.java
@@ -0,0 +1,21 @@
+package gg.blackdaber.sport.anotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@RequestMapping("/api")
+public @interface SportApiMapping {
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "value"
+    )
+    String[] value() default {};
+}
